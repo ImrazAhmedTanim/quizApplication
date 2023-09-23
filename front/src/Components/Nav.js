b7f99572a701ffe8458ddd9340213d5e19bac554
ImrazAhmedTanim <tanimf1@gmail.com>
2023-09-23 17:32:43 +0600

    Your commit message here

--- front/src/Components/Nav.js
+++ front/src/Components/Nav.js
@@ -10,7 +10,7 @@ export default function Nav() {
         <li>
           <Link to="/" className="brand">
             <img src="./images/logo-bg.png" alt="Learn with Sumit Logo" />
-            <h3>Learn with Sumit</h3>
+            <h3>Learn with Tanim</h3>
           </Link>
         </li>
       </ul>
